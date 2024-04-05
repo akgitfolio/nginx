c4465a97162e1126d0feddcff51ce581698ac611
ak <akv.git@gmail.com>
2024-04-05 22:57:46 +0100

    websocket

---
+++ websocket/app.js
@@ -0,0 +1,11 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  console.log("New WebSocket connection");
+  ws.on("message", function incoming(message) {
+    console.log("Received: %s", message);
+  });
+  ws.send("Hello, WebSocket client!");
+});
